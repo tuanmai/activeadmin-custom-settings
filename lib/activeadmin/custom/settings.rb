96ffa4f223eab8454cac4c2d2366646aaf858705
Mai Anh Tuan <perhuman@gmail.com>
2015-10-09 18:16:27 +0700

    init gem

---
+++ lib/activeadmin/custom/settings.rb
@@ -0,0 +1,9 @@
+require "activeadmin/custom/settings/version"
+
+module Activeadmin
+  module Custom
+    module Settings
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/activeadmin/custom/settings/version.rb
@@ -0,0 +1,7 @@
+module Activeadmin
+  module Custom
+    module Settings
+      VERSION = "0.1.0"
+    end
+  end
+end
